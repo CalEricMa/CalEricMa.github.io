e61dcfbb4d8a982bd4a154401d347fe1d5cb7eb2
CalEricMa <162977359+CalEricMa@users.noreply.github.com>
2024-10-06 10:38:26 -0700

    updated meta
    added a post, and updated meta tag while im procrastinating :)

--- script.js
+++ script.js
@@ -9,25 +9,4 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   });
   
 
-document.addEventListener('scroll', function () {
-  const sections = document.querySelectorAll('section');
-  const navLinks = document.querySelectorAll('.side-menu ul li a');
 
-  let currentSection = '';
-
-  sections.forEach(section => {
-    const sectionTop = section.offsetTop - 150; // Adjust the offset as needed (150 here)
-    const sectionHeight = section.offsetHeight;
-    
-    if (window.pageYOffset >= sectionTop && window.pageYOffset < sectionTop + sectionHeight) {
-      currentSection = section.getAttribute('id');
-    }
-  });
-
-  navLinks.forEach(link => {
-    link.classList.remove('active');
-    if (link.getAttribute('href').includes(currentSection)) {
-      link.classList.add('active');
-    }
-  });
-});
